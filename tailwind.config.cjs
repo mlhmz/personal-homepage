391e6c4d69442dd92d3fd0b17972023d220ee7ee
Malek <malek.hamza@gmx.de>
2023-06-22 18:51:00 +0200

    Improve page layout with containers

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,6 +4,16 @@ const { fontFamily } = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+    },
     extend: {
       fontFamily: {
         sans: ["Inter Variable", ...fontFamily.sans],
