d0ba349b789323560fa76bc6cf97d17d71b045fa
Malek <malek.hamza@gmx.de>
2023-06-28 20:32:00 +0200

    Remove blog entirely

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,33 +1,15 @@
-import { defineCollection, z } from 'astro:content';
-
-const blog = defineCollection({
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z
-			.string()
-			.or(z.date())
-			.transform((val) => new Date(val)),
-		updatedDate: z
-			.string()
-			.optional()
-			.transform((str) => (str ? new Date(str) : undefined)),
-		heroImage: z.string().optional(),
-	}),
-});
+import { defineCollection, z } from "astro:content";
 
 const portfolio = defineCollection({
-	schema: z.object({
-		index: z.number().min(1),
-		name: z.string(),
-		technologies: z.string().array().optional(),
-		gitHubLink: z.string().url().optional(),
-		shortDescription: z.string(),
-	}),
+  schema: z.object({
+    index: z.number().min(1),
+    name: z.string(),
+    technologies: z.string().array().optional(),
+    gitHubLink: z.string().url().optional(),
+    shortDescription: z.string(),
+  }),
 });
 
-export const collections = { 
-	blog, 
-	portfolio };
+export const collections = {
+  portfolio,
+};
