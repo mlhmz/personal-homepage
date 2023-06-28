6290bf8af4422be482a95fbd96c57bffb8b4ed83
Malek <malek.hamza@gmx.de>
2023-06-28 15:38:27 +0200

    Update container width to be more distraction free

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,12 +7,9 @@ module.exports = {
   theme: {
     container: {
       center: true,
-      padding: {
-        DEFAULT: '1rem',
-        sm: '2rem',
-        lg: '4rem',
-        xl: '5rem',
-        '2xl': '6rem',
+      padding: "2rem",
+      screens: {
+        "md": "800px",
       },
     },
     extend: {
