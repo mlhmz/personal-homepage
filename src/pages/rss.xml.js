7dffabb67ab85d07ea6504d73272f3c5a1200055
Malek <malek.hamza@gmx.de>
2023-06-28 21:15:45 +0200

    Change rss feed to track projects

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,14 +3,14 @@ import { getCollection } from 'astro:content';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 
 export async function get(context) {
-	const posts = await getCollection('blog');
+	const projects = await getCollection('portfolio');
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.slug}/`,
+		items: projects.map((project) => ({
+			...project.data,
+			link: `/portfolio/${post.slug}/`,
 		})),
 	});
 }
