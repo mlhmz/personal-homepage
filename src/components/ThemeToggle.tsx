e48e61f8de927ccb5e3494351e8400298ba110bf
Malek <malek.hamza@gmx.de>
2023-06-28 22:14:44 +0200

    Improve theme toggle styling

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -7,27 +7,41 @@ export default function ThemeToggle() {
     localStorage.getItem(THEME_KEY) ?? "white"
   );
   const [isMounted, setIsMounted] = useState(false);
+  const [isChanging, setIsChanging] = useState(false);
 
   const themeToggle = () => setTheme(theme === "white" ? "dark" : "white");
 
   useEffect(() => {
+    setIsChanging(true);
     if (theme === "dark") {
       document.documentElement.classList.add("dark");
     } else {
       document.documentElement.classList.remove("dark");
     }
     localStorage.setItem(THEME_KEY, theme);
+    // Setting a timeout so the animation can actually be seen
+    setTimeout(() => {
+      setIsChanging(false);
+    }, 100);
   }, [theme]);
 
   useEffect(() => {
     setIsMounted(true);
   }, []);
 
-
   if (!isMounted) {
-    return <Icons.spinner className="animate-spin" aria-label="Initializing theme" />;
+    return (
+      <Icons.spinner className="animate-spin" aria-label="Initializing theme" />
+    );
   }
   return (
-    <a onClick={themeToggle}>{ theme === "white" ? <Icons.sun /> : <Icons.moon /> }</a>
-  )
+    <a
+      className={
+        "cursor-pointer" + (isChanging && theme == "white" && " animate-spin")
+      }
+      onClick={themeToggle}
+    >
+      {theme === "white" ? <Icons.sun /> : <Icons.moon />}
+    </a>
+  );
 }
