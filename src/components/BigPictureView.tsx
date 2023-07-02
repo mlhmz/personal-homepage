3ec08edf25168fd80575ae10aac4105deb0937d1
Malek Hamza <malek.hamza@gmx.de>
2023-07-03 00:07:15 +0200

    Improve mobile proportions on big picture view

--- src/components/BigPictureView.tsx
+++ src/components/BigPictureView.tsx
@@ -8,8 +8,8 @@ interface BigPictureViewProps {
 
 export default function BigPictureView({ imgUrl, onClose } : BigPictureViewProps) {
   return (
-    <div className="fixed left-1/2 top-1/2 z-10 h-auto w-screen -translate-x-1/2 -translate-y-1/2 bg-black bg-opacity-40 backdrop-blur-sm">
-      <div className="rounded-md p-56 shadow-md">
+    <div className="fixed left-1/2 top-1/2 z-10 h-screen xl:h-auto w-screen -translate-x-1/2 -translate-y-1/2 bg-black bg-opacity-40 backdrop-blur-sm">
+      <div className="rounded-md xl:md:p-56 shadow-md">
         <div className="flex w-full items-center justify-end">
           <a className="m-3 cursor-pointer">
             <Icons.x onClick={() => onClose()} />
