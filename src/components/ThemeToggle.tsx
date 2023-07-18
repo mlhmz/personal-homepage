7085d34c3572f5ba7222c405e4217e82b3f530f8
Malek <malek.hamza@gmx.de>
2023-07-18 20:20:16 +0200

    Remove unnecessary animation

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -5,22 +5,16 @@ export default function ThemeToggle() {
   const THEME_KEY = "theme";
   const [theme, setTheme] = useState(localStorage.getItem(THEME_KEY) ?? "white");
   const [isMounted, setIsMounted] = useState(false);
-  const [isChanging, setIsChanging] = useState(false);
 
   const themeToggle = () => setTheme(theme === "white" ? "dark" : "white");
 
   useEffect(() => {
-    setIsChanging(true);
     if (theme === "dark") {
       document.documentElement.classList.add("dark");
     } else {
       document.documentElement.classList.remove("dark");
     }
     localStorage.setItem(THEME_KEY, theme);
-    // Setting a timeout so the animation can actually be seen
-    setTimeout(() => {
-      setIsChanging(false);
-    }, 100);
   }, [theme]);
 
   useEffect(() => {
@@ -32,7 +26,7 @@ export default function ThemeToggle() {
   }
   return (
     <a
-      className={"cursor-pointer" + (isChanging && theme == "white" && " animate-spin ease-in-out")}
+      className={"cursor-pointer"}
       onClick={themeToggle}>
       {theme === "white" ? <Icons.sun /> : <Icons.moon />}
     </a>
